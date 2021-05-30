0fad12c75a1af704ad0ff2eb71d9ec5fb9c6c0cb
Ted <tgroth@buffalo.edu>
2021-05-30 12:13:12 -0400

    Updates to glycoOnto.rdf:
    - Added sulfotransferase subclasses to "Sulfotransferase" class in
    Glycogene function.
    - Made LacDiNAc as a subclass of "Capping".
    - Renamed PSA&HNK1 to just "HNK-1"
    
    Python script glycoOnto_create.py has imports from glycoOnto_utils.py
    and glycoOnto_classObjects.py.  They are imported at the top of
    glycoOnto_create.py and instantiate sets of functions and class
    definitions.  This cleans up the main ontology generating script and
    makes things easier to source.
    
    Pathway definitions, reaction rules, and glycogene functions have been
    updated in "glycoOnto_individuals_reactions.rdf".

--- glycoOnto_create.py
+++ glycoOnto_create.py
@@ -7,6 +7,10 @@ import re
 import pathlib
 from pathlib import Path
 
+#Script-specific imports:
+from glycoOnto_classObjects import *
+from glycoOnto_utils import *
+
 #Load ontology
 glycoOnto=get_ontology('./glycoOnto.rdf').load()
 
@@ -14,8 +18,6 @@ glycoOnto=get_ontology('./glycoOnto.rdf').load()
 classDict={x.name:x for x in glycoOnto.classes()}
 
 # Read finished glycoEnzymes:
-#glycoEnzDB_finished=pd.read_csv('./finishedGlycogenes.tsv',sep='\t',header=None).iloc[:,[3,6,7]]
-
 glycoEnzDB_finished=pd.read_csv('./finishedGlycogenes.tsv',sep='\t',header=0)
 
 ### Define ontology terms to parse: ###
@@ -23,146 +25,15 @@ ontoTerms=['Pathway','Function']
 annotationTerms=['Reactant','Product','Constraint']
 ### Reparse the table to only contain relevant fields ###
 glycoEnzDB_finished=glycoEnzDB_finished[['geneName']+ontoTerms+annotationTerms]
-
-### Map data table terms to ontology properties:
-# Association classes:
-class inPathway_A:
-    def __init__(self,inst,pth):
-        self.inst=inst
-        self.pth=pth
-        self.datatype='object'
-    def check_instance_of(self):
-        # Determines if a gene is an instance of a class:
-        return(self.pth in self.inst.inPathway)
-    def add(self):
-        self.inst.inPathway.append(self.pth)
-    def associate(self):
-        if self.check_instance_of() is False:
-            self.add()
-
-class isa_A:
-    def __init__(self,inst,obj):
-        self.inst=inst
-        self.pth=obj
-        self.datatype='object'
-    def check_instance_of(self):
-        # Determines if a gene is an instance of a class:
-        return(self.pth in self.inst.is_a)
-    def add(self):
-        self.inst.is_a.append(self.pth)
-    def associate(self):
-        if self.check_instance_of() is False:
-            self.add()
-
-#Annotation classes
-class hasSubstr_A:  
-    def __init__(self,inst,substr_string):
-        self.inst=inst
-        self.string=substr_string
-        self.datatype='string'
-    def check_instance_of(self):
-        # Determines if a gene is an instance of a class:
-        return(self.string in self.inst.hasSubstrate)
-    def add(self):
-        self.inst.hasSubstrate.append(self.string)
-    def associate(self):
-        if self.check_instance_of() is False:
-            self.add()
- 
-class hasProd_A:  
-    def __init__(self,inst,prod_string):
-        self.inst=inst
-        self.string=prod_string
-        self.datatype='string'
-    def check_instance_of(self):
-        # Determines if a gene is an instance of a class:
-        return(self.string in self.inst.hasProduct)
-    def add(self):
-        self.inst.hasProduct.append(self.string)
-    def associate(self):
-        if self.check_instance_of() is False:
-            self.add()
-
-class hasConstr_A:  
-    def __init__(self,inst,constr_string):
-        self.inst=inst
-        self.string=constr_string
-        self.datatype='string'
-    def check_instance_of(self):
-        # Determines if a gene is an instance of a class:
-        return(self.string in self.inst.hasConstraint)
-    def add(self):
-        self.inst.hasConstraint.append(self.string)
-    def associate(self):
-        if self.check_instance_of() is False:
-            self.add()
-
-# Associate Functions with Dictionary Mappings:
-ontoDict={
-        'Pathway':inPathway_A,
-        'Function':isa_A
-}
-
-annotDict={
-        'Reactant':hasSubstr_A,
-        'Product':hasProd_A,
-        'Constraint':hasConstr_A
-}
-
 #Fix "Pathway" and "Functions" to match with strings:
 glycoEnzDB_finished['Pathway']=[re.sub('\ ','_',x) for x in glycoEnzDB_finished['Pathway']]
 glycoEnzDB_finished['Function']=[re.sub('\ ','_',x) for x in glycoEnzDB_finished['Function']]
 
+### Check for missing/incorrect classes in ontology:
+for i,row in glycoEnzDB_finished.iterrows():
+    print_class_errors(i,row,classDict,glycoOnto)
+
 #Helper function to create instances:
-
-def get_inst(geneName,onto):
-    # Checks if instance is already present in the ontology
-    # Returns object corresponding to instance if present, 
-    #   otherwise returns None
-    ind=[x for x in onto.individuals() if x.name==geneName]
-    if len(ind)==0:
-        return(None)
-    else:
-        return(ind[0])
-
-def check_if_class(cl,onto):
-    # Determines if class is valid based on ontology
-    return(cl in [x.name for x in onto.classes()])
-
-def class_process(classList,onto):
-    # Creates two lists: one with valid classes, one with invalid ones:
-    validClasses=[c for c in classList if check_if_class(c,onto)]
-    invalidClasses=[c for c in classList if c not in classList]
-    return validClasses,invalidClasses
-
-def newClasses(geneName,cl_list,onto):
-    # Creates a list of classes that haven't been associated 
-    # with the gene being processed:
-    return([c for c in cl_list if not check_instance_of(geneName,c,onto)])
-
-def add_classes(geneInst,classDict,classList):
-    # Adds classes to existing instance:
-    [geneInst.is_a.append(classDict[x]) for x in classList]
-
-def add_instance(geneName,cl,classDict):
-    # Adds instance to ontology provided a list of classes:
-    #If gene is new instantiate it:
-    print(cl)
-    inst=classDict[cl](geneName)
-    return(inst)
-
-#def add_instance(geneName,firstClass):
-#    # Adds instance to ontology provided a list of classes:
-#    #If gene is new instantiate it:
-#    inst=firstClass(geneName)
-#    return(inst)
-
-#def add_instance(geneName,firstClass):
-#    # Adds instance to ontology provided a list of classes:
-#    #If gene is new instantiate it:
-#    inst=firstClass(geneName)
-#    return(inst)
-
 
 # Main iteration:
 
---
+++ glycoOnto_utils.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pandas as pd
+import pickle as pkl
+from owlready2 import *
+import re
+import pathlib
+from pathlib import Path
+
+def get_inst(geneName,onto):
+    # Checks if instance is already present in the ontology
+    # Returns object corresponding to instance if present, 
+    #   otherwise returns None
+    ind=[x for x in onto.individuals() if x.name==geneName]
+    if len(ind)==0:
+        return(None)
+    else:
+        return(ind[0])
+
+def check_if_class(cl,onto):
+    # Determines if class is valid based on ontology
+    return(cl in [x.name for x in onto.classes()])
+
+def class_process(classList,onto):
+    # Creates two lists: one with valid classes, one with invalid ones:
+    validClasses=[c for c in classList if check_if_class(c,onto)]
+    invalidClasses=[c for c in classList if c not in classList]
+    return validClasses,invalidClasses
+
+def newClasses(geneName,cl_list,onto):
+    # Creates a list of classes that haven't been associated 
+    # with the gene being processed:
+    return([c for c in cl_list if not check_instance_of(geneName,c,onto)])
+
+def add_classes(geneInst,classDict,classList):
+    # Adds classes to existing instance:
+    [geneInst.is_a.append(classDict[x]) for x in classList]
+
+def add_instance(geneName,cl,classDict):
+    # Adds instance to ontology provided a list of classes:
+    #If gene is new instantiate it:
+    print(cl)
+    inst=classDict[cl](geneName)
+    return(inst)
