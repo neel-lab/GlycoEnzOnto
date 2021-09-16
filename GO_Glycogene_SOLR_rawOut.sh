286561a87718b12884ee6f8d0c163fc41c7ff2f0
Ted <tgroth@buffalo.edu>
2021-09-16 12:34:36 -0400

    Adding Gene Ontology API script and glycogene ontological annotations

---
+++ GO_Glycogene_SOLR_rawOut.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SOLR Query Parameters:
+FL_STRING="bioentity_label%2Cannotation_class%2Cannotation_class_label%2Cdescription%2Cevidence_type%2Contology_class"
+
+if [ -e ggene_json_dta_new.json ]
+then
+	rm ggene_json_dta_new.json
+fi
+
+glycogenes=./finishedGlycogenes.tsv
+for GGENE in $(awk 'BEGIN{FS="\t"}FNR==1{next}{print $4}' $glycogenes)
+do
+	echo "Parsing ${GGENE}"
+	GOLR_QUERY=$(echo "http://golr-aux.geneontology.io/solr/select?qt=standard&?fq=document_category:%22annotation%22&q=*:*&fq=bioentity_label:%22${GGENE}%22&fq:taxon=%22NCBITaxon%3A9606%22&fq=evidence_type:%22IDA%22&&wt=json&indent=on&fl=${FL_STRING}")
+	echo ${GOLR_QUERY}
+	#Store GOlr glycogene query in JSON string
+	curl -X GET -s $(echo "${GOLR_QUERY}") | jq .response.docs | jq '. = (. | unique)' | jq .[] >> ggene_json_dta_new.json
+done
+
+jq -s 'group_by(.bioentity_label) | map({geneName:.[0].bioentity_label,GO_accs:map(.annotation_class)})' ggene_json_dta_new.json > ggene_json_aggData.json
