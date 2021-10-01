2a88f0a24181b302cbf8dce4d2d040ffe17032ef
Ted <tgroth@buffalo.edu>
2021-10-01 13:36:36 -0400

    Adding "testRuleParserObj.py" to the repo, which checks if all reaction rules can successfully be parsed as "reactionRule" interpreter objects from "ruleInterpreter.py". Latest results of this parsing are in "testRuleParserObj_output.txt".

---
+++ ruleProcess/testRuleParserObj.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from tokenClasses_lex import *
+from ruleInterpreter import *
+import traceback
+
+
+def test_wrapper(rule,ruleName):
+    '''
+    Prints header information and logs
+    outpu from 
+    '''
+    print('\n************TEST: %s************'%(ruleName))
+    print(f'Rule String: {rule}')
+    try:
+        res=lexer(rule)
+    except Exception as exc:
+        print('PARSING ERROR\nERROR MESSAGE:')
+        print(exc)
+    if 'unknown' in str(res):
+        print('PARSING FAILURE')
+    try:
+        resObj=reactionRule(res)
+    except Exception as exc:
+        print('INSTANTIATION ERROR')
+        print(exc)
+    print('Parser Results (reading left to right):')
+    for r in res:
+        print(r)
+    print('\n************END************')
+
+finishedGlycogenes=pd.read_csv('../finishedGlycogenes.tsv',sep='\t',index_col=None)
+for i,r in finishedGlycogenes.iterrows():
+    if r['Rules']=='no reaction':
+        continue
+    test_wrapper(r['Rules'],r['geneName']+' Rule')
