b9c5188e7741b64ea35bf2edd0ee0b3790f3fac1
Ted <tgroth@buffalo.edu>
2021-12-15 16:51:51 -0500

    GlycoOnto Updates to Ontology:
    GlycoOnto Updates:
    
    * O-xyl pathway now only refers to the initiation of CS/DS/HS
    Glycosaminoglycans.  This consists of XYLT1/2 for the addition of xylose
    to Ser, B4GALT7 and B3GALT6 for the subsequent addition of galactose
    residues, and B3GAT3 for the addition of glucuronic acid.  CS&DS and HS
    pathways were moved underneath extension and branching pathways.
    
    * Sugar Metabolism pathway was removed because none of the subclasses
    were assigned to glycogenes.
    
    * Nucleotide sugar transport and Nucleotide sugar synthesis were
    underneath the Donor Synthesis class.  Now the Nucleotide sugar
    transport class is a subclass of Transport.
    
    * Terminal_Gal_Sulfation was added as a capping pathway.  GAL3ST enzymes
    were classified as such.
    
    * CMP-Neu59AC_Synthesis was added as a subpathway for CMP-Neu5Ac
    synthesis to incorporate the two metabolic enzymes CASD1 and SIAE.
    
    Gene Ontology parsing updates:
    
    * GO Biological Processes AND Molecular functions are parsed for each
    glycogene now.  Only the annotations with experimentally valid evidence
    codes were accepted.  Adding new script to repository
    "GO_Glycogene_SOLR_rawOut.sh"

--- GO_Glycogene_SOLR_rawOut.sh
+++ GO_Glycogene_SOLR_rawOut.sh
@@ -12,10 +12,11 @@ glycogenes=./finishedGlycogenes.tsv
 for GGENE in $(awk 'BEGIN{FS="\t"}FNR==1{next}{print $4}' $glycogenes)
 do
 	echo "Parsing ${GGENE}"
-	GOLR_QUERY=$(echo "http://golr-aux.geneontology.io/solr/select?qt=standard&?fq=document_category:%22annotation%22&q=*:*&fq=bioentity_label:%22${GGENE}%22&fq:taxon=%22NCBITaxon%3A9606%22&fq=evidence_type:%22IDA%22&&wt=json&indent=on&fl=${FL_STRING}")
+	GOLR_QUERY=$(echo "http://golr-aux.geneontology.io/solr/select?qt=standard&?fq=document_category:%22annotation%22&q=*:*&fq=bioentity_label:%22${GGENE}%22&fq:taxon=%22NCBITaxon%3A9606%22&&&wt=json&indent=on&fl=${FL_STRING}")
 	echo ${GOLR_QUERY}
 	#Store GOlr glycogene query in JSON string
-	curl -X GET -s $(echo "${GOLR_QUERY}") | jq .response.docs | jq '. = (. | unique)' | jq .[] >> ggene_json_dta_new.json
+	#curl -X GET -s $(echo "${GOLR_QUERY}") | jq .response.docs | jq '. = (. | unique)' | jq .[] >> ggene_json_dta_new.json
+	curl -X GET -s $(echo "${GOLR_QUERY}") | jq 'def IN(s): first((s == .) // empty) // false; ["EXP","IDA","IPI","IMP","IGI","IEP"] as $codes | .response.docs | map(select(.evidence_type | IN($codes[]))) | .[]' >> ggene_json_dta_new.json
 done
 
 jq -s 'group_by(.bioentity_label) | map({geneName:.[0].bioentity_label,GO_accs:map(.annotation_class)})' ggene_json_dta_new.json > ggene_json_aggData.json
